58b5369ef0b8c3bd2aeb04f6a82d15487623a7d7
James Churchill <james@smashdev.com>
2017-12-13 10:10:54 -0800

    Added component and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,21 @@ import { NgModule } from '@angular/core';
 //import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { MyTestComponent } from './my-test/my-test.component';
+import { TestContentService } from './test-content.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTestComponent
   ],
   imports: [
     BrowserModule
     // ,
     // AppRoutingModule
   ],
-  providers: [],
+  providers: [TestContentService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/my-test/my-test.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { TestContentService } from '../test-content.service';
+
+@Component({
+  selector: 'app-my-test',
+  templateUrl: './my-test.component.html',
+  styleUrls: ['./my-test.component.css']
+})
+export class MyTestComponent implements OnInit {
+  testContent: string;
+
+  constructor(private testContentService: TestContentService) { }
+
+  ngOnInit() {
+    this.testContent = this.testContentService.getContent();
+  }
+}
